980d1aacfd7abac8004a42494b32a9e42301d441
Marcus Beck <mbeck@tbep.org>
2024-06-04 16:30:01 +0000

    version 1.0.2

--- R/ebase_prep.R
+++ R/ebase_prep.R
@@ -7,7 +7,7 @@
 #' @param interval timestep interval in seconds
 #' @param ndays numeric for number of days in \code{dat} for optimizing the metabolic equation, see details
 #' 
-#' @details Checks if all columns are present by matching those in \code{\link{exdat}}, converts dissolved oxygen from mg/L to mmol/m3, calculates the Schmidt number (unitless) from water temp (C) and salinity (psu), and calculates dissolved oxygen equilibrium concentration (mmol/m3) from salinity and temperature
+#' @details Checks if all columns are present by matching those in \code{\link{exdat}}, checks if \code{DateTimeStamp} is in ascending order, converts dissolved oxygen from mg/L to mmol/m3, calculates the Schmidt number (unitless) from water temp (C) and salinity (psu), and calculates dissolved oxygen equilibrium concentration (mmol/m3) from salinity and temperature
 #' 
 #' The \code{ndays} argument defines the number of days that are used for optimizing the above mass balance equation.  By default, this is done each day, i.e., \code{ndays= 1} such that a loop is used that applies the model equation to observations within each day, evaluated iteratively from the first observation in a day to the last.  Individual parameter estimates for \emph{a}, \emph{R}, and \emph{b} are then returned for each day.  However, more days can be used to estimate the unknown parameters, such that the loop can be evaluated for every \code{ndays} specified by the argument.  The \code{ndays} argument will separate the input data into groups of consecutive days, where each group has a total number of days equal to \code{ndays}.  The final block may not include the complete number of days specified by \code{ndays} if the number of unique dates in the input data includes a remainder when divided by \code{ndays}, e.g., if seven days are in the input data and \code{ndays = 5}, there will be two groups where the first has five days and the second has two days. The output data from \code{\link{ebase}} includes a column that specifies the grouping that was used based on \code{ndays}.
 #' 
@@ -45,9 +45,13 @@ ebase_prep <- function(dat, Z, interval, ndays = 1){
     stop(msg)
   }
 
-  # sort dat by DateTimeStamp, add Z
+  # check if datetimestamp in ascending order
+  chk <- all(diff(dat$DateTimeStamp) > 0)
+  if(!chk)
+    stop('DateTimeStamp is not in ascending order')
+  
+  # add Z
   dat <- dat %>% 
-    dplyr::arrange(DateTimeStamp) %>% 
     dplyr::mutate(
       Z = Z
     )
@@ -113,35 +117,36 @@ ebase_prep <- function(dat, Z, interval, ndays = 1){
 
   }
   
-  # convert DO to mmol/m2
-  # add schmidt number as unitless
-  # add DO sat as mmol/m2
-  dat <- dat %>% 
-    dplyr::mutate(
-      DO_obs = Z * DO_obs / 32 * 1000,
-      sc = ebase_schmidt(Temp, Sal), 
-      DO_sat = Z * ebase_eqboxy(Temp, Sal), 
-      Date = as.Date(DateTimeStamp, tz = attr(DateTimeStamp, 'tzone'))
-    ) %>% 
-    dplyr::select(Date, DateTimeStamp, isinterp, DO_obs, DO_sat, Z, dplyr::everything())
-  
   # add groups defined by ndays to out
   # its an even cut by ndays with remainder assigned
-  out <- dat %>% 
+  dat <- dat %>% 
     dplyr::mutate(
+      Date = as.Date(DateTimeStamp, tz = attr(DateTimeStamp, 'tzone')),
       grp = as.numeric(Date),
       grp = grp - min(grp) + 1
     )
-
-  if(ndays >= length(unique(out$grp))) {
-    out$grp <- 1
+  
+  if(ndays >= length(unique(dat$grp))) {
+    dat$grp <- 1
   } else {
-    brks <- seq(min(out$grp), max(out$grp), by = ndays)
-    out$grp <- as.numeric(cut(out$grp, breaks = brks, right = FALSE))
-    maxgrp <- unique(out$grp) %>% max(na.rm = T)
+    brks <- seq(min(dat$grp), max(dat$grp), by = ndays)
+    dat$grp <- as.numeric(cut(dat$grp, breaks = brks, right = FALSE))
+    maxgrp <- unique(dat$grp) %>% max(na.rm = T)
     maxgrp <- 1 + maxgrp
-    out$grp <- ifelse(is.na(out$grp), maxgrp, out$grp)
+    dat$grp <- ifelse(is.na(dat$grp), maxgrp, dat$grp)
   }
+  
+  # convert DO to mmol/m3
+  # add schmidt number as unitless
+  # add DO sat as mmol/m3
+  out <- dat %>% 
+    dplyr::mutate(
+      DO_obs = DO_obs / 32 * 1000,
+      sc = ebase_schmidt(Temp, Sal), 
+      DO_sat = ebase_eqboxy(Temp, Sal), 
+    ) %>% 
+    dplyr::select(Date, DateTimeStamp, isinterp, DO_obs, DO_sat, Z, dplyr::everything()) |> 
+    dplyr::relocate(grp, .after = dplyr::last_col())
   
   return(out)
   
